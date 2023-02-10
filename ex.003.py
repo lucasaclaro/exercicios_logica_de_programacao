b23122fadf10c84af7f1c6433fab36e666dfc8d1
Lucas Claro <102222870+lucasaclaro@users.noreply.github.com>
2023-02-10 06:51:21 -0300

    Update ex.003.py

--- ex.003.py
+++ ex.003.py
@@ -44,32 +44,3 @@ elif current_month == months:
 print(f'Você viveu até hoje {total} dias.')
 
 
-#Escreva um algorítimo para ler o número total de municípios, o número de votos branco, nulos e válidos.
-#Calcule e escreva o percentual que cada representa em relação ao total de eleitores.
-
-total = int(input('Digite o número total de eleitores do município: '))
-brancos = int(input('Digite o número total de votos brancos: '))
-nulos = int(input('Digite o número total de votos nulos: '))
-validos = int(input('Digite o número total de votos válidos: '))
-
-percentual_brancos = (100 * brancos) / total
-percentual_nulos = (100 * nulos) / total
-percentual_validos = (100 * validos) / total
-
-print(f'''
-    O percentual de votos foi de: \n 
-    Brancos: {percentual_brancos:.2f}%,\n
-    Nulos: {percentual_nulos:.2f}%,\n
-    Válidos: {percentual_validos:.2f}%.
-    ''')
-
-
-
-#Escreva um algoritmo para ler o salário mensal atual de um funcionário e
-#o percentual de reajuste. Calcular e escrever o valor do novo salário.
-
-wage = float(input('Digite o salário atual: R$ '))
-readjustment = int(input('Digite o percentual de reajuste do salário (em %): '))
-new_wage = wage + (wage * readjustment / 100)
-
-print(f'Com o reajuste de {readjustment}%, o salário passará de R$ {wage} para R$ {new_wage:.2f}.')
